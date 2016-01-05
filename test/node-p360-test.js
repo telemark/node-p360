d56709e50403ef6678b17e15531d8a61b077b52d
maccyber <jonas.enge@gmail.com>
2016-01-05 10:23:09 +0100

    Added tests for required options

--- test/node-p360-test.js
+++ test/node-p360-test.js
@@ -12,9 +12,64 @@ tap.test('It requires an options object to be supplied', function (test) {
   })
 })
 
+tap.test('It requires an options object to be supplied', function (test) {
+  var options = {
+    p360: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.p360'
+  n360(options, function (error, data) {
+    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It requires an options object to be supplied', function (test) {
+  var options = {
+    p360: true,
+    clientService: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.clientService'
+  n360(options, function (error, data) {
+    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It requires an options object to be supplied', function (test) {
+  var options = {
+    p360: true,
+    clientService: true,
+    clientMethod: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.clientMethod'
+  n360(options, function (error, data) {
+    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('It requires an options object to be supplied', function (test) {
+  var options = {
+    p360: true,
+    clientService: true,
+    clientMethod: true,
+    args: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.args'
+  n360(options, function (error, data) {
+    test.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
 tap.throws(
   function withNoCallback () {
-    var options = {'gotOptions': 'hellYeah!'}
+    var options = {
+      p360: true,
+      clientService: true,
+      clientMethod: true,
+      args: true
+    }
     n360(options)
   },
   {message: 'Missing required input: callback'},
