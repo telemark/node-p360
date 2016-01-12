ba3166d88a9c0c5cb9b9d95573ffcde94e4e5855
Jonas Enge <jonas.enge@t-fk.no>
2016-01-12 19:25:52 +0100

    Wrong clientService name

--- examples/synchronizeEnterprise.js
+++ examples/synchronizeEnterprise.js
@@ -2,7 +2,7 @@
 
 var p360 = require('../index')
 var config = require('../config')
-var clientService = 'contacts'
+var clientService = 'ContactService'
 var clientMethod = 'SynchronizeEnterprise'
 var args = {
   'parameter': {
