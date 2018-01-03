0cf01deca11fb5bf51641e7b499d36639f81b226
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-01-03 08:56:30 +0100

    Updates Node. Updates tests (minor)

--- test/modules/dev-dependencies-test.js
+++ test/modules/dev-dependencies-test.js
@@ -1,7 +1,7 @@
 const test = require('ava')
 const pkg = require('../../package.json')
-const dependencies = pkg.dependencies || {}
-const dropModules = []
+const dependencies = pkg.devDependencies || {}
+const dropModules = ['doctoc', 'nsp']
 const isDropped = (module) => !dropModules.includes(module)
 
 if (Object.keys(dependencies).length > 0) {
